76240741e56bc2a3d5dd0e52c203c74201f1f565
ParkMinJae <yoo11052@naver.com>
2021-01-21 20:45:31 +0900

    2nd Commit

--- MyProject/src/SqlExam.java
+++ MyProject/src/SqlExam.java
@@ -1,66 +1,61 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class SqlExam {
 	
-		String url = "jdbc:mysql://localhost/mysql?serverTimezone=UTC";
-		String user = "root";
-		String passwd = "1234";
+	static Connection connection = null;
+	
+	static {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/mysql?serverTimezone=UTC","root","1234");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 
-	public <T> List<T> select(String query, ResultSetConsumer<T> consumer){
-		Connection con = null;
-		Statement st = null;
+	public <T> List<T> select(String sql, ResultSetConsumer<T> consumer){
 		ResultSet rs = null;
 		List<T> result = new ArrayList<T>();
-		try {
-			con = DriverManager.getConnection(url,user,passwd);
-			st = con.createStatement();
-
-			rs = st.executeQuery(query);
+		try (PreparedStatement pst = connection.prepareStatement(sql)){
+			rs = pst.executeQuery();
 			while(rs.next()) {
 				result.add(consumer.consume(rs));
 			}
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
-			try {
-				if(rs != null)
-					rs.close();
-				if(st != null)
-					st.close();
-				if(con != null)
-					con.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
 		}
 		return result;
 	}
 	
 	SqlExam() {
 		
-		List<String> list = select("select user from user", new ResultSetConsumer<String>() {
-			@Override
-			public String consume(ResultSet rs) {
-				try {
-					return rs.getString("user");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				return null;
+		
+		List<String> list = select("select * from tbl_bus", e -> {
+			try {
+				return e.getString("user");
+			} catch (Exception e2) {
+				e2.printStackTrace();
 			}
+			return null;
 		});
 			
+		func(list);
+	}
+	
+	private <T> void func(List<T> list) {
 		for(int i=0;i<list.size();i++) {
 			System.out.println(list.get(i));
 		}
-		
 	}
+	
 	
 	public static void main(String[] args) {
 		new SqlExam();
